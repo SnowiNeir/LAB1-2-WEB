8c1366beca29ab3b12d27b03ad9dc39f8b0a3168
A-Yukii <vladgnedasova@gmail.com>
2026-02-25 03:30:51 +0300

    Added

---
+++ 6.6.3.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Text;
+class HelloWorld
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        Console.WriteLine(" x  "); // заголовок таблицы 
+        int space = 0;
+        for (char x = 'R'; x >= 'A'; x--)
+        {
+            // Создаем строку которая будет добавлять 'R' - x пробелов
+            // Пример: R - R = 0(+ 0 пробелов), R - Q = 1(+1 пробел) и т.д.
+            Console.WriteLine("| {0}{1} |", new string(' ', 'R' - x), x);
+        }
+        Console.ReadKey();
+    }
+}
---
+++ 9.9.2.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Text;
+
+class HelloWorld
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        int[] array = new int[12];
+        Random rnd = new Random();
+
+        Console.WriteLine("Масcив: ");
+        for (int i = 0; i < 12; i++)
+        {
+            array[i] = rnd.Next(71);
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+
+        int arif = 0, count = 0;
+        double geom = 1;
+
+        for (int i = 0; i < 12; i++)
+        {
+            if (array[i] % 2 != 0)
+            {
+                arif += array[i];
+                geom *= array[i];
+                count++;
+            }
+        }
+
+        Console.WriteLine("Cреднее арифметическое нечетных элементов: " + arif / count);
+        Console.WriteLine("Cреднее геометрическое нечетных элементов: " + Math.Pow(geom, 1.0 / count)); // 1.0 надо, чтобы при 1/3 получилось 0.(3) и т.д., а не 0
+        Console.ReadKey();
+    }
+}
